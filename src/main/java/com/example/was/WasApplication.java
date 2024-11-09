c0fc76fcfba442ae19df52cd323d5929075daa45
yoonjae han <yoonjae.han@obigo.com>
2024-11-09 12:37:56 +0900

    add hello

--- src/main/java/com/example/was/WasApplication.java
+++ src/main/java/com/example/was/WasApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WasApplication {
 
     public static void main(String[] args) {
+        System.out.println("helo");
         SpringApplication.run(WasApplication.class, args);
     }
 
