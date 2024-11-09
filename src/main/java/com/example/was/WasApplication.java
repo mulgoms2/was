a85818838ec616177873419463a957e89ffb509b
yoonjae han <yoonjae.han@obigo.com>
2024-11-09 13:17:54 +0900

    add homePage

--- src/main/java/com/example/was/WasApplication.java
+++ src/main/java/com/example/was/WasApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WasApplication {
 
     public static void main(String[] args) {
-        System.out.println("helo");
-
         SpringApplication.run(WasApplication.class, args);
     }
 
---
+++ src/main/java/com/example/was/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.was.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @ResponseBody
+    @GetMapping("/")
+    public String home () {
+        return "hello";
+    }
+}
