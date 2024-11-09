1cac52093fe595cac7262c90e11ba62b4199eaab
yoonjae han <yoonjae.han@obigo.com>
2024-11-09 13:00:19 +0900

    add hello2

--- src/main/java/com/example/was/WasApplication.java
+++ src/main/java/com/example/was/WasApplication.java
@@ -8,6 +8,7 @@ public class WasApplication {
 
     public static void main(String[] args) {
         System.out.println("helo");
+
         SpringApplication.run(WasApplication.class, args);
     }
 
