c91f3b73a291a8e323fa6438057693f50e3b0d78
yoonjae han <yoonjae.han@obigo.com>
2024-12-09 17:16:05 +0900

    add cors config

---
+++ src/main/java/com/example/was/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.was.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://was.woowang.store") // 허용할 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
