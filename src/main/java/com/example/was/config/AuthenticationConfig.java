e0ca703fcd766952b66d8522387b737010d41862
yoonjae han <yoonjae.han@obigo.com>
2024-12-09 17:30:42 +0900

    add cors config

--- src/main/java/com/example/was/config/AuthenticationConfig.java
+++ src/main/java/com/example/was/config/AuthenticationConfig.java
@@ -1,5 +1,6 @@
 package com.example.was.config;
 
+import com.example.was.constants.ApiConstant;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -28,12 +34,26 @@ public class AuthenticationConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/api/v1/login", "/api/v1/user/valid", "/api/v1/users","/api/v1/healthCheck")
+                .authorizeHttpRequests((authorize) -> authorize.requestMatchers(ApiConstant.WHITE_LIST_URL)
                         .permitAll()
                         .anyRequest()
                         .authenticated());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://was.woowang.store"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     @Bean
---
+++ src/main/java/com/example/was/constants/ApiConstant.java
@@ -0,0 +1,10 @@
+package com.example.was.constants;
+
+public class ApiConstant {
+    public static final String[] WHITE_LIST_URL = {
+            "/api/v1/login",
+            "/api/v1/user/valid",
+            "/api/v1/users",
+            "/api/v1/healthCheck"
+    };
+}
