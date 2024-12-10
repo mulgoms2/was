409b301277be52d0386b6b0bb106dfc1f45e6660
yoonjae han <yoonjae.han@obigo.com>
2024-12-10 13:23:22 +0900

    change cors config

--- src/main/java/com/example/was/config/CorsConfig.java
+++ src/main/java/com/example/was/config/CorsConfig.java
@@ -31,8 +31,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000","https://woowangjwawang.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
-        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
