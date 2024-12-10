b73ff9d4c0ec8ba9760f9e3ae2d2d108ec286790
yoonjae han <yoonjae.han@obigo.com>
2024-12-10 12:56:52 +0900

    add cors filter

--- src/main/java/com/example/was/config/AuthenticationConfig.java
+++ src/main/java/com/example/was/config/AuthenticationConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 
@@ -20,7 +21,8 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 @RequiredArgsConstructor
 public class AuthenticationConfig {
-    private final CorsFilter corsFilter;
+//    private final CorsFilter corsFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
@@ -32,8 +34,8 @@ public class AuthenticationConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-//                .cors(cors -> cors.configurationSource(corsConfigurationSource))
-                .addFilter(corsFilter)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+//                .addFilter(corsFilter)
                 .authorizeHttpRequests((authorize) -> authorize.requestMatchers(ApiConstant.WHITE_LIST_URL)
                         .permitAll()
                         .anyRequest()
